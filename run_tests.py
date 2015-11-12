c5d971e0797509ce48805fc7b127a62889e8ea1b
Julien Pagès <j.parkouss@gmail.com>
2015-11-12 13:38:26 +0100

    final fixes
    - put back check_patches on top of the file
    - switched to single strings everywhere
    - removed useless except statement

--- run_tests.py
+++ run_tests.py
@@ -14,9 +14,14 @@ from subprocess import check_call
 import start
 
 
-DIR_TEST_ENV = "test/venv"
-DIR_JENKINS_ENV = "jenkins-env"
-VERSION_VIRTUALENV = "13.1.2"
+DIR_TEST_ENV = os.path.join('test', 'venv')
+DIR_JENKINS_ENV = 'jenkins-env'
+VERSION_VIRTUALENV = '13.1.2'
+
+
+def check_patches():
+    print 'Checking patches'
+    check_call(['./test/check_patches.sh'])
 
 
 class Jenkins(object):
@@ -32,8 +37,8 @@ class Jenkins(object):
         session = requests.Session()
 
         def wait_for_jenkins():
-            if not session.get("http://localhost:8080").status_code == 200:
-                raise Exception("Jenkins did not start successfully.")
+            if not session.get('http://localhost:8080').status_code == 200:
+                raise Exception('Jenkins did not start successfully.')
 
         redo.retry(wait_for_jenkins, sleeptime=0.5, jitter=0, sleepscale=1,
                    attempts=120)
@@ -45,7 +50,7 @@ class Jenkins(object):
 class VirtualEnv(object):
     def __init__(self):
         if os.path.exists(DIR_TEST_ENV):
-            print "Using virtual environment in ", DIR_TEST_ENV
+            print 'Using virtual environment in ', DIR_TEST_ENV
             return
 
         tar_fname = 'virtualenv-%s.tar.gz' % VERSION_VIRTUALENV
@@ -60,7 +65,7 @@ class VirtualEnv(object):
         tar = tarfile.open(name=tar_fname)
         tar.extractall(path='.')
         check_call([sys.executable,
-                    "virtualenv-%s/virtualenv.py" % VERSION_VIRTUALENV,
+                    'virtualenv-%s/virtualenv.py' % VERSION_VIRTUALENV,
                     DIR_TEST_ENV])
 
     def activate(self):
@@ -73,27 +78,24 @@ class VirtualEnv(object):
 
 
 def run_tests():
-    print "Checking patches"
-    check_call(["./test/check_patches.sh"])
+    check_patches()
 
     if not os.path.exists(DIR_JENKINS_ENV):
-        sys.exit("Jenkins env is not initialized. Please run './setup.sh'")
-    print "Starting Jenkins"
-    os.environ["JENKINS_HOME"] = "jenkins-master"
+        sys.exit('Jenkins env is not initialized. Please run "./setup.sh"')
+    print 'Starting Jenkins'
+    os.environ['JENKINS_HOME'] = 'jenkins-master'
     jenkins = Jenkins()
     try:
         jenkins.wait_for_started()
         venv = VirtualEnv()
         venv.activate()
         venv.run('pip', 'install', 'selenium')
-        venv.run('python', "test/configuration/save_config.py")
-    except Exception as exc:
-        sys.exit(str(exc))
+        venv.run('python', 'test/configuration/save_config.py')
     finally:
-        print "Killing Jenkins"
+        print 'Killing Jenkins'
         jenkins.kill()
 
-    check_call(["git", "--no-pager", "diff", "--exit-code"])
+    check_call(['git', '--no-pager', 'diff', '--exit-code'])
 
 
 if __name__ == '__main__':
